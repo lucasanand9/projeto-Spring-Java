e5e79ad150b3e084ca95c4a6da738d63a3cebaff
lucasanand9 <lucasanand9@gmail.com>
2022-05-04 10:06:15 -0300

    adicionar

--- src/main/java/br/senai/controller/FuncionarioController.java
+++ src/main/java/br/senai/controller/FuncionarioController.java
@@ -1,13 +1,26 @@
 package br.senai.controller;
 
+import br.senai.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FuncionarioController {
-@GetMapping("/funcionario/list")
-    public String findAll(){
+
+    @Autowired
+    FuncionarioService funcionarioService;
+
+    @GetMapping("/funcionario/list")
+    public String findAll(Model model){
+        model.addAttribute("funcionarios", funcionarioService.findALL());
     return "funcionario/list";
 }
 
+    @GetMapping("/funcionario/add")
+    public String add(){
+        return "funcionario/add";
+
+    }
 }
