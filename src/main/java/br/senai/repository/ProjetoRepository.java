d2eb579cd94d4731fd2787c4129ddea0ac940b36
lucasanand9 <lucasanand9@gmail.com>
2022-05-25 10:12:53 -0300

    projetos

---
+++ src/main/java/br/senai/repository/ProjetoRepository.java
@@ -0,0 +1,10 @@
+package br.senai.repository;
+
+import br.senai.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+
+}
---
+++ src/main/java/br/senai/service/ProjetoService.java
@@ -0,0 +1,10 @@
+package br.senai.service;
+
+import br.senai.model.Projeto;
+
+import java.util.List;
+
+public interface ProjetoService {
+    public List<Projeto> findAll();
+    public  Projeto save(Projeto projeto);
+}
